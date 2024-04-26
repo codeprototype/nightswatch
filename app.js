ffc576b81faad53003ce045326c03029a958b952
codeprototype <maaz.abdullah@techmojo.in>
2024-04-26 18:55:36 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+import cors from "cors";
+import path from "path";
+const dirname = path
+  .dirname(new URL(import.meta.url).pathname)
+  .split("/development")
+  .pop();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    sucess: true,
+    data: "NA",
+    message: `Healthcheck OK, Service Name:${dirname}`,
+  });
+});
+// import chatRoute from "./routes/chatRoutes.js"
+// app.use(chatRoute)
+
+const PORT = process.env.PORT || 9005;
+
+app.listen(PORT, () => console.log("Server running at port " + PORT));
