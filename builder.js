b67f304976748c76003c2f224d9d53c58f1f37c5
codeprototype <maaz.abdullah@techmojo.in>
2024-04-30 19:35:02 +0530

    builders and rangers changed

--- builder.js
+++ builder.js
@@ -1,19 +1,7 @@
-import jwt from 'jsonwebtoken'
-function builders(secret) {
-    return function(req, res, next) {
-        const token = req.headers.authorization;
-        if (!token) {
-            return res.status(401).json({ error: 'No token provided' });
-        }
+import jwt from 'jsonwebtoken';
 
-        jwt.verify(token, secret, (err, decoded) => {
-            if (err) {
-                return res.status(403).json({ error: 'Failed to authenticate token' });
-            }
-            req.user = decoded;
-            next();
-        });
-    };
+function builders(data, secret='the king in the north') {
+    return jwt.sign(data, secret);
 }
 
 export default builders;
--- ranger.js
+++ ranger.js
@@ -1,19 +1,6 @@
-import jwt from 'jsonwebtoken'
-function rangers(secret) {
-    return function(req, res, next) {
-        const token = req.headers.authorization;
-        if (!token) {
-            return res.status(401).json({ error: 'No token provided' });
-        }
-
-        jwt.verify(token, secret, (err, decoded) => {
-            if (err) {
-                return res.status(403).json({ error: 'Failed to authenticate token' });
-            }
-            req.user = decoded;
-            next();
-        });
-    };
+import jwt from "jsonwebtoken";
+function rangers(token, secret = "the king in the north") {
+  return jwt.verify(token, secret);
 }
 
 export default rangers;
